666b4f7325f6b400f7495ab1c4b71f926176945a
Thomas Mesquita <thomasm@xcellentcreations.com>
2012-11-28 21:22:19 -0700

    generate rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Soundboard::Application.config.secret_token = 'eeaf228962b46b0307861dc7454ac0fa4891f02371c94089a610022c754b163fbdab99c73a049175fcbc237923851bd5556e1f695ed4c6aea44c53d47e8e3fb2'
