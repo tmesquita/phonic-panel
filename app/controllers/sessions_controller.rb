cccbaa91246a036c12914afdc5da12dd0ab69bff
Thomas Mesquita <thomasm@xcellentcreations.com>
2012-12-05 23:18:49 -0700

    removing debugging statements

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,11 @@
 class SessionsController < ApplicationController
   def create
     authorization = request.env['omniauth.auth']
-    ap authorization
+
     unless @auth = Authorization.find_with_omniauth(authorization)
-      # Create a new user or add an auth to existing user, depending on
-      # whether there is already a user signed in.
       @auth = Authorization.create_with_omniauth(authorization, current_user)
     end
-    # Log the authorizing user in.
+
     self.current_user = @auth.user
 
     render :text => "Welcome, #{current_user.name}."
